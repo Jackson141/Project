a9ec083f4c158a1f770384c5a0575bfc4901f89c
Jackson141 <dasjackson66@gmail.com>
2019-11-26 10:11:27 +0530

    first commit

---
+++ scripts/ques.data.js
@@ -0,0 +1,25 @@
+var set = [
+        {
+            'hello' : ["hello", "hi", "hey there", "hey, how's you?"],
+            'howareyou' : ["good", "good, you say.", "fine, tell yours.", "marvelous 😜", "awesome" ],
+            'good' : ["good to hear 😊", "good to know", "great 👍🏻"],
+            'fine' : ["good to know 😊", "great 👍🏻", "that's great"],
+            'bye' : ["okay bye", "good bye", "bye", "goodbye", "tata", "see you later", "see you again"],
+            'thankyou' : ["welcome", "glad to help you", "you're welcome"],
+            'ok' : ["hmm", "okay", "yeah 👍🏻"],
+            'whatdoyoudo' : ['I can help you.', 'I am here to assist you.','researching and telling you what I am doing today.. helping you.'],
+            'whoareyou' : ['I am your personal assistant'],
+            'whatiss&pglobal': ['S&P Global Market Intelligence. S&P Global Market Intelligence is a provider of multi-asset class and real-time data, research news and analytics to institutional investers and investment.<a class="innerLinks" target="_blank" href="https://www.spglobal.com/marketintelligence/en/">Read more..</a>'],
+            'whoisdouglaspeterson' : ['CEO of S&P Global, formerly McGraw Hill Financial.'],
+            'goodmorning' : ["gm", "good morning", "good morning buddy"],
+            'goodevening' : ["good evng", "good evening", "good evening"],
+            'goodafternoon' : ["good afternoon", "good afternoon buddy", "very good afternoon"],
+            'fromwhichsourceyouareaddingormaintainingcompanyaddress' : ['company website and company provided documents.'],
+            'howyoumaintainnativecompanies' : ['as we have decided fixed procedure to maintain that.'],
+            'fromwhichsourcebusinessdescriptionyouareadding' : ['as per line of business given in documents and website.'],
+            'howweareaddingshareholders' : ['it is also added from documents and website.'],
+            'whatifinshareholderlisttrustandindividualsgiven' : ['we are not going to add as per our fixed procedure.'],
+            'howyouaremaintainingsubsidiary' : ['it is different-different procure for domain wise.'],
+            'whatkindofdatayouaremaintaining' : ['as hierarchy management we are covering corporate data only']
+        }
+    ];
